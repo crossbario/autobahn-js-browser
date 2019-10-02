e64bed1a0796e19d666643b3eccfd3f60f1bc07c
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-10-02 20:30:30 +0200

    add rel v19.10.1

--- autobahn.js
+++ autobahn.js
@@ -462,7 +462,7 @@ var Connection = function (options) {
    self._max_retries = typeof self._options.max_retries !== 'undefined' ?  self._options.max_retries : 15;
 
    // initial retry delay in seconds
-   self._initial_retry_delay = self._options.initial_retry_delay || 1.5;
+   self._initial_retry_delay = typeof self._options.initial_retry_delay !== 'undefined' ? self._options.initial_retry_delay : 1.5;
 
    // maximum seconds between reconnection attempts
    self._max_retry_delay = self._options.max_retry_delay || 300;
@@ -499,9 +499,9 @@ var Connection = function (options) {
 
 
 Connection.prototype._create_transport = function () {
-   
+
    var self = this;
-   
+
    for (var i = 0; i < this._transport_factories.length; ++i) {
       var transport_factory = this._transport_factories[i];
       log.debug("trying to create WAMP transport of type: " + transport_factory.type);
@@ -525,9 +525,9 @@ Connection.prototype._create_transport = function () {
 Connection.prototype._init_transport_factories = function () {
     // WAMP transport
     //
-    
+
     var self = this;
-   
+
     var transports, transport_options, transport_factory, transport_factory_klass;
 
     util.assert(this._options.transports, "No transport.factory specified");
@@ -727,21 +727,24 @@ Connection.prototype.open = function () {
 
          var next_retry = self._autoreconnect_advance();
 
+         var details = {
+            reason: self._session_close_reason,
+            message: self._session_close_message,
+            retry_delay: next_retry.delay,
+            retry_count: next_retry.count,
+            will_retry: next_retry.will_retry
+         };
+
+         log.warn("connection closed", reason, details);
+
          // fire app code handler
          //
          if (self.onclose) {
-            var details = {
-               reason: self._session_close_reason,
-               message: self._session_close_message,
-               retry_delay: next_retry.delay,
-               retry_count: next_retry.count,
-               will_retry: next_retry.will_retry
-            };
             try {
                // Connection.onclose() allows to cancel any subsequent retry attempt
                var stop_retrying = self.onclose(reason, details);
             } catch (e) {
-                util.handle_error(self._options.on_user_error, e, "Exception raised from app code while firing Connection.onclose()");
+               util.handle_error(self._options.on_user_error, e, "Exception raised from app code while firing Connection.onclose()");
             }
          }
 
@@ -762,12 +765,14 @@ Connection.prototype.open = function () {
 
                self._is_retrying = true;
 
-               log.debug("retrying in " + next_retry.delay + " s");
+               log.warn("auto-reconnecting in " + next_retry.delay + "s ..");
                self._retry_timer = setTimeout(retry, next_retry.delay * 1000);
 
             } else {
-               log.debug("giving up trying to reconnect");
+               log.warn("giving up trying to auto-reconnect!");
             }
+         } else {
+            log.warn("auto-reconnect disabled!", self._retry, stop_retrying);
          }
       }
    }
@@ -5269,6 +5274,72 @@ var http_post = function (url, data, timeout) {
    }
 };
 
+// Helper to do HTTP/GET requests returning JSON parsed result as a promise.
+var http_get_json = function (url, timeout) {
+
+   var d = when.defer();
+   var req = new XMLHttpRequest();
+   req.withCredentials = true; // pass along cookies
+   req.onreadystatechange = function () {
+
+      if (req.readyState === 4) {
+
+         // Normalize IE's response to HTTP 204 when Win error 1223.
+         // http://stackoverflow.com/a/10047236/884770
+         //
+         var status = (req.status === 1223) ? 204 : req.status;
+
+         if (status === 200) {
+
+            // parse response
+            var data = JSON.parse(req.responseText);
+
+            // response with content
+            //
+            d.resolve(data);
+
+         } if (status === 204) {
+
+            // empty response
+            //
+            d.resolve();
+
+         } else {
+
+            // anything else is a fail
+            //
+            var statusText = null;
+            try {
+               statusText = req.statusText;
+            } catch (e) {
+               // IE8 fucks up on this
+            }
+            d.reject({code: status, text: statusText});
+         }
+      }
+   }
+
+   req.open("GET", url, true);
+   req.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+   if (timeout > 0) {
+      req.timeout = timeout; // request timeout in ms
+
+      req.ontimeout = function () {
+         d.reject({code: 501, text: "request timeout"});
+      }
+   }
+
+   req.send();
+
+   if (d.promise.then) {
+      // whenjs has the actual user promise in an attribute
+      return d.promise;
+   } else {
+      return d;
+   }
+};
+
 /**
  * Merge a list of objects from left to right
  *
@@ -5429,6 +5500,7 @@ exports.handle_error = handle_error;
 exports.rand_normal = rand_normal;
 exports.assert = assert;
 exports.http_post = http_post;
+exports.http_get_json = http_get_json;
 exports.defaults = defaults;
 exports.new_global_id = new_global_id;
 exports.deferred_factory = deferred_factory;
@@ -25757,7 +25829,7 @@ define(function (require) {
 },{"./lib/Promise":96,"./lib/TimeoutError":98,"./lib/apply":99,"./lib/decorators/array":100,"./lib/decorators/flow":101,"./lib/decorators/fold":102,"./lib/decorators/inspect":103,"./lib/decorators/iterate":104,"./lib/decorators/progress":105,"./lib/decorators/timed":106,"./lib/decorators/unhandledRejection":107,"./lib/decorators/with":108}],120:[function(require,module,exports){
 module.exports={
   "name": "autobahn",
-  "version": "19.9.2",
+  "version": "19.10.1",
   "description": "An implementation of The Web Application Messaging Protocol (WAMP).",
   "main": "index.js",
   "files": [
@@ -25787,9 +25859,9 @@ module.exports={
     "random-bytes-seed": ">=1.0.3"
   },
   "browser": {
+    "cbor": false,
     "ws": false,
-    "./lib/transport/rawsocket.js": false,
-    "cbor": false
+    "./lib/transport/rawsocket.js": false
   },
   "repository": {
     "type": "git",
